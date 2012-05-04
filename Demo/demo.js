2e56f83958ec07b02fd200a607bd07d8afad89f3
Lukáš Vlček <lukas.vlcek@gmail.com>
2012-05-04 09:57:28 +0200

    add simple chart (inspired by http://jsfiddle.net/Y5q8H/50/)

--- Demo/demo.js
+++ Demo/demo.js
@@ -1,11 +1,15 @@
-window.addEvent('domready', function() {
-	new Request.HTML({
-		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
-		data: {'delay': 1},
-		method: 'post',
-		update: 'demo',
-		onSuccess: function(response) {
-			$('demo').highlight();
-		}
-	}).send();
-})
+var chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'container',
+        defaultSeriesType: 'line',
+        zoomType: 'x'
+    },
+
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+
+    series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }]
+});​
